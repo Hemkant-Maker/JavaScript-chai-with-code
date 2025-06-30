4496866c06f5cd37e96306fdc03f39ad670e60af
Hemkant-Maker <135342640+Hemkant-Maker@users.noreply.github.com>
2025-06-30 17:18:08 +0530

    Update 01_Variables.js

--- 01_Basics/01_Variables.js
+++ 01_Basics/01_Variables.js
@@ -3,3 +3,8 @@ let accountEmail = "Hemkant@gmail.com"
 var accountpassword = "1234"
 accountCity = "Jaipur"
 accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accuntcity = "Bangaluru"
+
+
+Console.log(accountId);
